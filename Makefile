a8c10434b67f6c182f9a20d372898380e5ec1688
Daniel da Rosa <danieldarosati@gmail.com>
2019-11-24 13:43:03 -0300

    add testes validando o input de ingredientes

--- Makefile
+++ Makefile
@@ -1,1 +1,11 @@
 SHELL = /bin/sh
+include app.env
+export $(shell sed 's/=.*//' app.env)
+
+.PHONY: test
+test:
+	npm test
+
+.PHONY: start
+start:
+	node app.js
---
+++ test/integration.js
@@ -0,0 +1,42 @@
+var chai = require('chai')
+  , expect = chai.expect
+  , chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+var app = require('../app');
+
+it('should response 200 code', function(done) { 
+  chai.request(app)
+  .get('/recipes/?i=chayote')
+  .end(function(err, res) {
+    expect(res).to.have.status(200);
+    done();
+  });
+});
+
+it('should accept 3 parameters', function(done) { 
+  chai.request(app)
+  .get('/recipes/?i=chayote,eggplant,carrot')
+  .end(function(err, res) {
+    expect(res).to.have.status(200);
+    done();
+  });
+});
+
+it('should return 400 when receive 0 parameters', function(done) { 
+  chai.request(app)
+  .get('/recipes/')
+  .end(function(err, res) {
+    expect(res).to.have.status(400);
+    done();
+  });
+});
+
+it('should return 400 when receive more than 3 parameters', function(done) { 
+  chai.request(app)
+  .get('/recipes/?i=chayote,eggplant,carrot,egg')
+  .end(function(err, res) {
+    expect(res).to.have.status(400);
+    done();
+  });
+});
