3d6c5e2b76792bdd0b3dc13e1c09c330c1665a91
Daniel da Rosa <danieldarosati@gmail.com>
2019-11-24 23:34:40 -0300

    add link do gif nas receitas

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const router = express.router;
-const recipeRoute = require('./routes/recipe')
+const recipeRoute = require('./routes/recipe');
 
-app.use('/recipes', recipeRoute)
+app.use('/recipes', recipeRoute);
 
 app.listen(process.env.API_PORT, () => {
   console.log('running at: ' + process.env.API_PORT);
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+const rp = require('request-promise');
+
 const fetchRecipeUrl = process.env.FETCH_RECIPE_URL;
-const service = require('../services/recipe-service');
+const fetchGiphyUrl = process.env.FETCH_GIPHY_URL;
+const giphyApiKey = process.env.GIPHY_API_KEY;
 
 router.get('/', (req, res) => {
-
   let ingredients;
   try {
     ingredients = req.query.i ? req.query.i.split(',') : [];
-    
+
     if (!ingredients.length || ingredients.length > 3) {
       console.log('invalid amount of ingredients: ' + ingredients.length);
       res.status(400).send('Bad Request');
@@ -20,40 +21,54 @@ router.get('/', (req, res) => {
   	res.status(400).send('Bad Request');
   }
 
-  ingredients.sort()
-  let target = fetchRecipeUrl + '?i=' + ingredients;
+  ingredients.sort();
+  const targetRecipe = fetchRecipeUrl + '?i=' + ingredients;
 
-  request(target, (error, response, body) => {
-  	if(response.statusCode >= 500) {
-  		console.error('target ' + target + ' responsed with status: ' + response.statusCode);
-  		res.status(500).send('recipe service is unavailable!');
+  rp(targetRecipe)
+      .then((response) => {
+    	return JSON.parse(response).results;
+      })
+  	.then((results) => {
+  	  const promises = [];
+  	  results.map((r) => {
+  	  	const i = r.ingredients.split(',');
+  	  	const treated = {
+  	  	  title: r.title,
+       	  ingredients: i,
+       	  link: r.href,
+       	};
+  	  	promises.push(fetchGifs(treated));
+  	  });
+  	  return promises;
+  	}).then((promises) => {
+  		return Promise.all(promises);
+  	}).then((recipeList) => {
+  		const r = {
+          keywords: ingredients,
+          recipes: recipeList,
+  		};
+  		res.send(r);
+  	})
+  	.catch((error) => {
+  		let msg = 'fails attempting get recipes!';
+  		if (error.statusCode >= 500) {
+  		  msg = 'recipe service is unavailable!';
+  	    }
+
+  		console.error('fails attempting get recipes: ', error.statusCode);
+  		res.status(error.statusCode).send(msg);
   		return;
-  	}
-
-  	if(error) {
-  		console.error('target ' + target + ' responsed with error: ' + error);
-  		res.status(500).send('recipe service is unavailable!');
-  		return;
-  	}
-
-  	let recipes = JSON.parse(body).results;
-  	let results = recipes.map((el) => {
-  	  let i = el.ingredients.split(',');
-
-      return {
-      	title: el.title,
-      	ingredients: i,
-      	link: el.href,
-      }
   	});
-
-  	let treated = {
-  		keywords: ingredients,
-  		recipes: results
-  	};
-
-  	res.send(treated);
-  });
 });
+
+fetchGifs = (result) => {
+  const targetGiphy = 'http://' + fetchGiphyUrl + '?api_key=' + giphyApiKey + '&tag=' + result.title;
+  return rp(targetGiphy)
+      .then((gif) => {
+        const url = JSON.parse(gif).data.url;
+  	  result.gif = url;
+  	  return result;
+      });
+};
 
 module.exports = router;
