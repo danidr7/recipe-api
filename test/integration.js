459e73067a662bb4794d1550e9cff7c3d19e5a77
Daniel da Rosa <daniel.rosa@neoway.com.br>
2019-11-24 23:34:40 -0300

    add link do gif nas receitas

--- test/integration.js
+++ test/integration.js
@@ -1,53 +1,64 @@
-var chai = require('chai')
-  , expect = chai.expect
-  , chaiHttp = require('chai-http');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-var app = require('../app');
+const app = require('../app');
 
-it('should response 200 code', function(done) { 
+it('should response 200 code', function(done) {
   chai.request(app)
-  .get('/recipes/?i=chayote')
-  .end(function(err, res) {
-    expect(res).to.have.status(200);
-    done();
-  });
+      .get('/recipes/?i=chayote')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
 });
 
-it('should accept 3 parameters', function(done) { 
+it('should accept 3 parameters', function(done) {
   chai.request(app)
-  .get('/recipes/?i=chayote,eggplant,carrot')
-  .end(function(err, res) {
-    expect(res).to.have.status(200);
-    done();
-  });
+      .get('/recipes/?i=chayote,eggplant,carrot')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
 });
 
-it('should return 400 when receive 0 parameters', function(done) { 
+it('should return 400 when receive 0 parameters', function(done) {
   chai.request(app)
-  .get('/recipes/')
-  .end(function(err, res) {
-    expect(res).to.have.status(400);
-    done();
-  });
+      .get('/recipes/')
+      .end(function(err, res) {
+        expect(res).to.have.status(400);
+        done();
+      });
 });
 
-it('should return 400 when receive more than 3 parameters', function(done) { 
+it('should return 400 when receive more than 3 parameters', function(done) {
   chai.request(app)
-  .get('/recipes/?i=chayote,eggplant,carrot,egg')
-  .end(function(err, res) {
-    expect(res).to.have.status(400);
-    done();
-  });
+      .get('/recipes/?i=chayote,eggplant,carrot,egg')
+      .end(function(err, res) {
+        expect(res).to.have.status(400);
+        done();
+      });
 });
 
-it('should return sorted keywords', function(done) { 
+it('should return sorted keywords', function(done) {
   chai.request(app)
-  .get('/recipes/?i=chayote,eggplant,carrot')
-  .end(function(err, res) {
-    expect(res.body.keywords[0]).to.equal('carrot');
-    expect(res.body.keywords[1]).to.equal('chayote');
-    expect(res.body.keywords[2]).to.equal('eggplant');
-    done();
-  });
+      .get('/recipes/?i=chayote,eggplant,carrot')
+      .end(function(err, res) {
+        expect(res.body.keywords[0]).to.equal('carrot');
+        expect(res.body.keywords[1]).to.equal('chayote');
+        expect(res.body.keywords[2]).to.equal('eggplant');
+        done();
+      });
+});
+
+it('should return recipes with a gif', function(done) {
+  chai.request(app)
+      .get('/recipes/?i=milk,banana')
+      .end(function(err, res) {
+  	res.body.recipes.forEach((item) => {
+          expect(item).to.not.be.empty;
+  	});
+        done();
+      });
 });
