d05a547bfbfcf359bfd3b006e560198ed7614c84
Daniel da Rosa <danieldarosati@gmail.com>
2019-11-25 18:33:28 -0300

    add o middleware de log morgan

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const recipeRoute = require('./routes/recipe');
 const env = require('./conf/env');
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
 
 app.use('/recipes', recipeRoute);
+
 
 app.listen(env.apiPort, () => {
   console.log('running at: ' + env.apiPort);
